e12300ceb0b74a93ff595f0df092fcf2bc6d1f2e
unknown <kayajohnson71@yahoo.com>
2020-06-24 18:41:15 -0400

    Updated Practice Notes

--- DataTypes/InClassPractice/6.24.2020.js
+++ DataTypes/InClassPractice/6.24.2020.js
@@ -4,13 +4,58 @@
 // level 0: using .map() do the following
     // double the values of any array of numbers
 
+    let numArr = [1,2,3,4,5,6,7];
+
+    let doubleValue = numArr.map( ele => ele * 2)
+
+    console.log(doubleValue)
+
+
 // level 1: using .map() do the following
     // grab the first letter of an array of strings
-    // grab the first element of an array of arrays
+
+    // let strArr = ['hello','world','!'];
+
+    // let getFirst = strArr.map(ele => ele[0])
+
+    // console.log(getFirst)
+
+// console.log(strArr[1][0])
+// console.log(strArr[0][0])
+// console.log(strArr[2][0])
+
+
+// grab the first element of an array of arrays
+
+
+//  let arrOfArr = [['blue','green'],['pink','gold'],['yellow','black']]
+
+//  let getFirst = arrOfArr.map(ele => ele[0])
+
+//  console.log(getFirst)
+
+
 
 // level 2: using .map() do the following
-    // grab the last letter of an array of strings
+    
+// grab the last letter of an array of strings
+
+    let arrOfStr = ['hello','world','whats','goin','on','today','!'];
+
+    let getLast = arrOfStr.map(ele => ele[4])
+
+    console.log(arrOfStr.length -1)
+    console.log(arrOfStr[arrOfStr.length -1])
+    console.log(arrOfStr[6])
+
+
+
+
     // grab the last letter of an array of arrays
+
+
+
+
 
 // level 3: using .map() do the following
     // create a new array of fullNames from an array of objects where ONE object looks similar to this:{first: 'Nic', last: 'Leask'};
